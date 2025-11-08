ffc95e097da54c7a27da8bf108d50ca2409b7a9a
Aman-Pal287 <palaman9786@gmail.com>
2025-11-08 06:33:17 +0530

    feat: update API base URL to production endpoint and configure static file serving

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,12 @@ const cors = require("cors");
 const productRoutes = require("./routes/product.route");
 const cartRoutes = require("./routes/cart.routes");
 const checkoutRoutes = require("./routes/checkout.route");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.json({ message: "Product Service is up and running" });
@@ -15,5 +17,9 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
